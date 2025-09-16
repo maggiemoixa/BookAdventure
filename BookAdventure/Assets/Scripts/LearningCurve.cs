0e42021a29eaa52c02088ca3bfae8d8b2a66cec9
maggiemoixa <music3frog@gmail.com>
2025-09-15 23:31:43 -0500

    chapter 4

--- BookAdventure/Assets/Scripts/LearningCurve.cs
+++ BookAdventure/Assets/Scripts/LearningCurve.cs
@@ -18,11 +18,24 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public string FirstName = "Maggie";
     public string CharacterName = "Doodoo";
     public bool IsAuthor = true;
+    public bool PureOfHeart = true;
+    public bool HasSecretIncantation = false;
+    public string RareItem = "Relic Stone";
+    public string CharacterAction = "Attack";
 
+    List<string> QuestPartyMembers = new List<string>()
+    {
+    "Grim the Barbarian",
+    "Merlin the Wise",
+    "Sterling the Knight"
+    };
 
-
-
-
+    Dictionary<string, int> ItemInventory = new Dictionary<string, int>()
+    {
+        { "Potion", 5 },
+        { "Antidote", 7 },
+        { "Aspirin", 1 }
+    };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,7 +45,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
         Debug.Log(NextSkillLevel);
         Debug.Log(GenerateCharacter("Faye", characterLevel));
         ComputeAge();
+        OpenTreasureChamber();
 
+        PrintCharacterAction();
+        QuestPartyMembers.Add("Craven the Necromancer");
+       
+        Debug.LogFormat("Party Members: {0}", QuestPartyMembers.Count);
+
+        Debug.LogFormat("Items: {0}", ItemInventory.Count);
 
         // The dollar sign $ lets you embed variables into a string!!!
         Debug.Log($"{FirstName} wrote this code!");
@@ -46,14 +66,50 @@ public class NewMonoBehaviourScript : MonoBehaviour
         return level += 5;
     }
 
+    public void PrintCharacterAction()
+    {
+        switch (CharacterAction)
+        {
+            case "Potion":
+            case "Heal":
+            Debug.Log("Potion sent.");
+            break;
+
+            case "Attack":
+                Debug.Log("To arms!");
+            break;
+
+        default:
+                Debug.Log("Shields up.");
+            break;
+    }
+    }
+
     void ComputeAge()
     {         
         Debug.Log(CurrentAge + AddedAge);
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void OpenTreasureChamber()
+    {
+        if (PureOfHeart && RareItem == "Relic Stone")
+        {
+            if (!HasSecretIncantation)
+            {
+                Debug.Log("You have the spirit, but not the knowledge.");
+            }
+            else
+            {
+                Debug.Log("The treasure is yours, worthy hero!");
+            }
+        }
+        else
+        {
+            Debug.Log("Come back when you have what it takes.");
+        }
+    }
+        // Update is called once per frame
+        void Update()
     {
         
     }
