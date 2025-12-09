9d1fe009072dbb792d8533d641ee0fce0a13d2d0
maggiemoixa <music3frog@gmail.com>
2025-12-08 20:38:59 -0600

    chapter 12
    encountered some issues with pages 362-364

--- BookAdventure/Assets/Scripts/DataManager.cs
+++ BookAdventure/Assets/Scripts/DataManager.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+
 
 public class DataManager : MonoBehaviour
 {
@@ -11,6 +16,22 @@ public class DataManager : MonoBehaviour
         set { _state = value; }
     }
 
+    private string _dataPath;
+    private string _textFile;
+    private string _streamingTextFile;
+    private string _xmlLevelProgress;
+
+
+    void Awake()
+    {
+        _dataPath = Application.persistentDataPath + "/Player_Data/";
+        Debug.Log(_dataPath);
+
+        _textFile = _dataPath + "Save_Data.txt";
+        _streamingTextFile = _dataPath + "Streaming_Save_Data.txt";
+        _xmlLevelProgress = _dataPath + "Progress_Data.xml";
+
+    }
 
     void Start()
     {
@@ -21,5 +42,154 @@ public void Initialize()
     {
         _state = "Data Manager initialized..";
         Debug.Log(_state);
+
+        FileSystemInfo();
+        NewDirectory();
+        WriteToStream(_streamingTextFile);
+        ReadFromStream(_streamingTextFile);
+        WriteToXML(_xmlLevelProgress);
+        
+
+       // NewDirectory();
+       // FileSystemInfo();
+       // NewDirectory();
+       // NewTextFile();
+       // UpdateTextFile();
+       // ReadFromFile(_textFile);
     }
+
+    public void FileSystemInfo()
+    {
+        Debug.LogFormat("Path separator character: {0}",
+        Path.PathSeparator);
+        Debug.LogFormat("Directory separator character: {0}",
+        Path.DirectorySeparatorChar);
+        Debug.LogFormat("Current directory: {0}",
+        Directory.GetCurrentDirectory());
+        Debug.LogFormat("Temporary path: {0}",
+        Path.GetTempPath());
+    }
+
+    public void NewDirectory()
+    {
+    
+        if(Directory.Exists(_dataPath))
+        {
+        Debug.Log("Directory already exists...");
+        return;
+        }
+
+        Directory.CreateDirectory(_dataPath);
+        Debug.Log("New directory created!");
+    }
+
+
+    public void DeleteDirectory()
+    {
+        if(!Directory.Exists(_dataPath))
+        {
+            Debug.Log("Directory doesn't exist or has already been deleted...");
+        
+            return;
+        }
+
+        Directory.Delete(_dataPath, true);
+        Debug.Log("Directory successfully deleted!");
+    }
+
+    public void NewTextFile()
+        {
+            if (File.Exists(_textFile))
+            {
+                Debug.Log("File already exists...");
+                return;
+            }
+            File.WriteAllText(_textFile, "<SAVE DATA>\n");
+            Debug.Log("New file created!");
+        }
+
+
+        public void UpdateTextFile()
+            {
+            if (!File.Exists(_textFile))
+            {
+                Debug.Log("File doesn't exist...");
+                return;
+            }
+            File.AppendAllText(_textFile, $"Game started: {DateTime.Now}\n");
+
+            Debug.Log("File updated successfully!");
+            }
+
+        public void ReadFromFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                Debug.Log("File doesn't exist...");
+                return;
+            }
+            Debug.Log(File.ReadAllText(filename));
+        }
+
+
+        public void DeleteFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                Debug.Log("File doesn't exist or has already been deleted...");
+                return;
+            }
+            File.Delete(_textFile);
+            Debug.Log("File successfully deleted!");
+        }
+
+        public void WriteToStream(string filename)
+        {
+        
+        if (!File.Exists(filename))
+        {
+       
+            StreamWriter newStream = File.CreateText(filename);
+           
+            newStream.WriteLine("<Save Data> for HERO BORN \n");
+            newStream.Close();
+            Debug.Log("New file created with StreamWriter!");
+        }
+
+        StreamWriter streamWriter = File.AppendText(filename);
+      
+        streamWriter.WriteLine("Game ended: " + DateTime.Now);
+        streamWriter.Close();
+        Debug.Log("File contents updated with StreamWriter!");
+        }
+
+        public void ReadFromStream(string filename)
+        {
+        if (!File.Exists(filename))
+        {
+            Debug.Log("File doesn't exist...");
+            return;
+        }
+        StreamReader streamReader = new StreamReader(filename);
+        Debug.Log(streamReader.ReadToEnd());
+        }
+
+        public void WriteToXML(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                FileStream xmlStream = File.Create(filename);
+                XmlWriter xmlWriter = XmlWriter.Create(xmlStream);
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("level_progress");
+
+                for (int i = 1; i < 5; i++)
+                {
+                xmlWriter.WriteElementString("level", "Level-" + i);
+                }
+                xmlWriter.WriteEndElement();
+                xmlWriter.Close();
+                xmlStream.Close();
+            }
+        }
 }
--- BookAdventure/Assets/Scripts/Weapon.cs
+++ BookAdventure/Assets/Scripts/Weapon.cs
@@ -1,5 +1,7 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public struct Weapon
 {
